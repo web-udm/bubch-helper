8cc2e6ae3bd5b32c8ba20526fee103b0d6f33b70
moildar <i.mosnabeev@taxi-master.com>
2020-12-28 16:11:46 +0400

    Тестовая настройка проекта

--- public/index.php
+++ public/index.php
@@ -1,3 +1,19 @@
 <?php
 
-echo 'hello world';
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\RequestInterface as Request;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addErrorMiddleware(true, false, false);
+
+$app->get('/test', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write('Hello, Slim');
+
+    return $response;
+});
+
+$app->run();
