391e5c60ea77876e327994246d0fff1727af815f
moildar <web.udm@gmail.com>
2021-01-19 23:08:51 +0400

    Небольшие правки в контроллер

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,27 +2,49 @@
 
 namespace App\Controllers;
 
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use App\Services\VkSenderService;
+use Slim\App;
+use Slim\Views\Twig;
 
 class HomeController
 {
     /**
+     * @var ContainerInterface
+     */
+    public ContainerInterface $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
      * @param RequestInterface $request
      * @param ResponseInterface $response
+     * @param array $groupsIDs
+     * @param int $numberOfPosts
      * @return ResponseInterface
      * @throws \GuzzleHttp\Exception\GuzzleException
      */
-    public function home (RequestInterface $request,
-                          ResponseInterface $response,
-                          array $groupsIDs,
-                          int $numberOfPosts) : ResponseInterface
+    public function results(RequestInterface $request,
+                            ResponseInterface $response,
+                            array $groupsIDs,
+                            int $numberOfPosts) : ResponseInterface
     {
         $sender = new VkSenderService();
 
         foreach ($groupsIDs as $groupsId) {
-            $sender->getPosts($numberOfPosts, $groupsId);
+            $res = $sender->getPosts($numberOfPosts, $groupsId);
         }
+    }
+
+    public function home(RequestInterface $request, ResponseInterface $response) : ResponseInterface
+    {
+
+
+        return $this->container->get('view')->render($response, 'home/home.twig');
     }
 }
