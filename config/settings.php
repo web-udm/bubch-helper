6fdc43b4116447f64667e2c7ee89fd8e70b7e932
moildar <web.udm@gmail.com>
2021-01-24 15:45:50 +0400

    Вынес некоторые настройки в конфиги

--- config/settings.php
+++ config/settings.php
@@ -3,4 +3,10 @@
 return
     [
         'production' => false,
+
+        'api_settings' => [
+            'api_version' => '5.21',
+            'api_token' => 'a0930e90685e661bff94872193a114d4cbbd2a5a485486de346f243d4a2c22f8b481e730c89532d1a7676',
+            'api_url' => 'https://api.vk.com/method/',
+        ]
     ];
