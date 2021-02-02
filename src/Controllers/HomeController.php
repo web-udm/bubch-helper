c88b6e050b63e6e9d3b2d01669f680bd72bfc05d
moildar <i.mosnabeev@taxi-master.com>
2021-02-02 18:30:22 +0400

    Добавил возможность указать кол-во постов на главной странице

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -37,9 +37,17 @@ class HomeController
     {
         try {
             $links = explode("\n", $request->getParsedBody()['links']);
+            $postsNumber = ($request->getParsedBody()['posts_number']);
 
             if (empty($links[0])) {
                 throw new \Exception('<p>Ты забыла записать ссылки</p>');
+            }
+
+            if ($postsNumber == 'миллион') {
+                throw new \Exception(
+                    "<p>Ты сломала сервер</p>" .
+                            "<img src='http://ww2.sinaimg.cn/bmiddle/9150e4e5ly1fh3mcehmamg2088088qlf.gif'>"
+                );
             }
 
             $posts = [];
@@ -52,19 +60,14 @@ class HomeController
                 }
 
                 $groupId = trim($match[1]); //убираем символ переноса строки, обусловлено вводом пользователя в форму
-                $posts[] = $this->vkSender->getPosts($groupId, 1);
+                $posts[] = $this->vkSender->getPosts($groupId, $postsNumber);
             }
 
             $serializePosts = $this->postSerializer->serialize($posts);
 
-            /*echo '<pre>';
-            var_dump($serializePosts);
-            var_dump($posts);
-            echo '</pre>';*/
-
             return $this->twig->render($response, 'home/results.twig',['groupsData' => $serializePosts]);
         } catch (\Exception $e) {
-            $response->getBody()->write('<p>Бубчи, что-то пошло не так :C: </p>' . $e->getMessage());
+            $response->getBody()->write('<p>Бубчи, что-то пошло не так: </p>' . $e->getMessage());
 
             return $response;
         }
